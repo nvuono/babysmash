b63141346d43623b3bbd985a75fb75dc9ca4c397
nvuono <nvuono@yahoo.com>
2019-03-06 22:45:08 -0500

    Cleaned up pixel format handling and colorization. CoolImage class still has more responsibility than is ideal but we'll see where the game design takes us before changing anything.

--- Shapes/CoolImage.xaml.cs
+++ Shapes/CoolImage.xaml.cs
@@ -12,11 +12,16 @@ namespace BabySmash
 {
     /// <summary>   
     /// Interaction logic for CoolImage.xaml
+    /// Includes image colorization logic
     /// </summary>
     [Serializable]
     public partial class CoolImage
     {
         private static Random rnd = new Random();
+
+        // store a copy of what was used to generate the image in case we want to ever backtrack for game/interaction purposes
+        private VocabularyImage vocabularyImage = null;
+        private System.Windows.Media.Color? imageColor = null;
 
         public CoolImage()
         {
@@ -25,23 +30,88 @@ namespace BabySmash
         
         public CoolImage(System.Windows.Media.Brush x, string itemName, System.Windows.Media.Color templateColor) : this()
         {
-            var vocabImage = Controller.ImageVocab.GetVocabularyImageForWord(itemName);
-            if (vocabImage.ImageBytes!=null) { 
-                Stream imageStream = new MemoryStream(vocabImage.ImageBytes);
-                PngBitmapDecoder decoder = new PngBitmapDecoder(imageStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-                BitmapSource myBitmapSource = decoder.Frames[0];
-                WriteableBitmap wBmp = new WriteableBitmap(myBitmapSource);
+            // store copy of parameters used to generate this image
+            vocabularyImage = Controller.ImageVocab.GetVocabularyImageForWord(itemName);
+            imageColor = templateColor;
 
-                int iWidth = (int)decoder.Frames[0].Width;
-                int iHeight = (int)decoder.Frames[0].Height;
-                byte[] iPixel = new byte[iWidth * iHeight * 8];
-                decoder.Frames[0].CopyPixels(iPixel, iWidth * 8, 0);
+            BitmapFrame bitmapFrame = null; // used for colorizing and ultimately drawing to screen
+            
+            if (vocabularyImage.ImageBytes!=null) { 
+                Stream imageStream = new MemoryStream(vocabularyImage.ImageBytes);
+                PngBitmapDecoder decoder = new PngBitmapDecoder(imageStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                bitmapFrame = decoder.Frames[0];
+            }
+            else if (vocabularyImage.EmojiChar!=null)
+            {
+                bitmapFrame = GetBitmapFrameForEmoji(vocabularyImage.EmojiChar);
+            }
+
+            if (bitmapFrame != null)
+            {
+                // if everything worked correctly up to this point we should have a real bitmapFrame
+                int iWidth = (int)bitmapFrame.PixelWidth;
+                int iHeight = (int)bitmapFrame.PixelHeight;
+                Int32 stride = (bitmapFrame.PixelWidth * bitmapFrame.Format.BitsPerPixel + 7) / 8;
+
+                byte[] iPixel = ApplyColoring(templateColor, bitmapFrame, iWidth, iHeight);
+
+                BitmapSource myBitmapSource = bitmapFrame;
+                WriteableBitmap wBmp = new WriteableBitmap(myBitmapSource);
+                wBmp.WritePixels(new Int32Rect(0, 0, iWidth, iHeight), iPixel, stride, 0);
+                MyImage.Source = wBmp;
+            }
+            else
+            {
+                // if no bitmapFrame then fallback to just drawing a letter
+                this.Character = itemName[0];
+                this.letterPath.Fill = x;
+
+                this.letterPath.Data = MakeCharacterGeometry(GetLetterCharacter(itemName[0]));
+                this.Width = this.letterPath.Data.Bounds.Width + this.letterPath.Data.Bounds.X + this.letterPath.StrokeThickness / 2;
+                this.Height = this.letterPath.Data.Bounds.Height + this.letterPath.Data.Bounds.Y + this.letterPath.StrokeThickness / 2;
+            }
+        }
+
+        private BitmapFrame GetBitmapFrameForEmoji(string emojiString)
+        {
+            BitmapFrame frame;
+            string fontName = "Segoe UI Emoji"; // trying options
+            fontName = "Segoe Color Emoji"; // can't seem to get the color emojis to render 
+
+            FormattedText text = new FormattedText(emojiString,
+                new CultureInfo("en-us"), FlowDirection.LeftToRight,
+                new Typeface(new FontFamily(fontName), FontStyles.Normal, FontWeights.Normal, new FontStretch()),
+                100, this.Foreground);
+
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawText(text, new Point(2, 2));
+            drawingContext.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap(180, 180, 120, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+
+            var bitmapEncoder = new PngBitmapEncoder();
+            bitmapEncoder.Frames.Add(BitmapFrame.Create(bmp));
+            frame = bitmapEncoder.Frames[0];
+            return frame;
+        }
+
+        private static byte[] ApplyColoring(Color templateColor, BitmapFrame frame, int iWidth, int iHeight)
+        {
+            byte[] iPixel = new byte[iWidth * iHeight * 8];
+            Int32 stride = (frame.PixelWidth * frame.Format.BitsPerPixel + 7) / 8;
+            if (frame.Format == PixelFormats.Rgba64)
+            {
+                // PNG images for my vocabulary.zip file are typically in the RGBA64 format
+                frame.CopyPixels(iPixel, stride, 0);
 
                 for (int i = 0; i < iPixel.Length; i += 8)
                 {
+                    int weightThreshold = 1000;
                     uint pixelContent = (((uint)iPixel[i]) << 8) + iPixel[i + 1] + (((uint)iPixel[i + 2]) << 8) + iPixel[i + 3] + (((uint)iPixel[i + 4]) << 8) + iPixel[i + 5];
 
-                    if (pixelContent > 1000)
+                    if (pixelContent > weightThreshold)
                     {
                         iPixel[i] = (byte)((((UInt16)(templateColor.ScR * 65535)) & 0xFF00) >> 8);
                         iPixel[i + 1] = (byte)((((UInt16)(templateColor.ScR * 65535)) & 0xFF));
@@ -56,46 +126,17 @@ namespace BabySmash
                         iPixel[i + 7] = (byte)((((UInt16)(templateColor.ScA * 65535)) & 0xFF)); ;
                     }
                 }
-                wBmp.WritePixels(new Int32Rect(0, 0, iWidth, iHeight), iPixel, iWidth * 8, 0);
-                MyImage.Source = wBmp;
-            }
-            else if (vocabImage.EmojiChar!=null)
+            }else if (frame.Format == PixelFormats.Pbgra32)
             {
-                string fontName = "Segoe UI Emoji";
-                fontName = "Segoe Color Emoji";
-;
-                FormattedText text = new FormattedText(vocabImage.EmojiChar,
-                    new CultureInfo("en-us"),
-                    FlowDirection.LeftToRight,
-                    new Typeface(new FontFamily(fontName), FontStyles.Normal, FontWeights.Normal, new FontStretch()),
-                    100,
-                    this.Foreground);
-
-                DrawingVisual drawingVisual = new DrawingVisual();
-                DrawingContext drawingContext = drawingVisual.RenderOpen();
-                drawingContext.DrawText(text, new Point(2, 2));
-                drawingContext.Close();
-
-                RenderTargetBitmap bmp = new RenderTargetBitmap(180, 180, 120, 96, PixelFormats.Pbgra32);
-                bmp.Render(drawingVisual);
-
-                var bitmapEncoder = new PngBitmapEncoder();
-                bitmapEncoder.Frames.Add(BitmapFrame.Create(bmp));
-
-                BitmapSource myBitmapSource = bitmapEncoder.Frames[0];
-                WriteableBitmap wBmp = new WriteableBitmap(myBitmapSource);
-
-                int iWidth = (int)bitmapEncoder.Frames[0].PixelWidth;
-                int iHeight = (int)bitmapEncoder.Frames[0].PixelHeight;
-                Int32 stride = (bitmapEncoder.Frames[0].PixelWidth * bitmapEncoder.Frames[0].Format.BitsPerPixel + 7) / 8;
-                byte[] iPixel = new byte[iHeight * stride];
-                bitmapEncoder.Frames[0].CopyPixels(iPixel, stride, 0);
+                iPixel = new byte[iHeight * stride];
+                frame.CopyPixels(iPixel, stride, 0);
 
                 for (int i = 0; i < iPixel.Length; i += 4)
                 {
+                    int weightThreshold = 2; // if pixel RGB add up to more than 2 we treat it as a 'black' pixel that can be colorized based on templatecolor
                     uint pixelContent = (uint)iPixel[i] + (uint)iPixel[i + 1] + +(uint)iPixel[i + 2] + +(uint)iPixel[i + 3]; //+ (uint)iPixel[i + 2] + (uint)iPixel[i + 3];//(((uint)iPixel[i]) << 8) + iPixel[i + 1] + (((uint)iPixel[i + 2]) << 8) + iPixel[i + 3] + (((uint)iPixel[i + 4]) << 8) + iPixel[i + 5];
 
-                    if (pixelContent > 2)
+                    if (pixelContent > weightThreshold)
                     {
                         iPixel[i + 0] = (byte)(templateColor.ScB * 255);
                         iPixel[i + 1] = (byte)(templateColor.ScG * 255);
@@ -103,19 +144,13 @@ namespace BabySmash
                         iPixel[i + 3] = (byte)(templateColor.ScA * 255); ;
                     }
                 }
-                wBmp.WritePixels(new Int32Rect(0, 0, iWidth, iHeight), iPixel, stride, 0);
-                MyImage.Source = wBmp;
-
             }
             else
             {
-                this.Character = itemName[0];
-                this.letterPath.Fill = x;
-
-                this.letterPath.Data = MakeCharacterGeometry(GetLetterCharacter(itemName[0]));
-                this.Width = this.letterPath.Data.Bounds.Width + this.letterPath.Data.Bounds.X + this.letterPath.StrokeThickness / 2;
-                this.Height = this.letterPath.Data.Bounds.Height + this.letterPath.Data.Bounds.Y + this.letterPath.StrokeThickness / 2;
+                System.Diagnostics.Debug.WriteLine("Unsupported format applying color to image: " + frame.Format);
+                throw new ArgumentException("Unsupported format applying color to image: " + frame.Format);
             }
+            return iPixel;
         }
 
         public char Character { get; private set; }
